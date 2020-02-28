1fd8c02719cd58b23eb17c4b388131d90548147e
Justin Gilley <justindgilley@gmail.com>
2020-02-27 23:38:11 -0600

    change mysql

--- server.js
+++ server.js
@@ -9,12 +9,10 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 var connection = mysql.createConnection({
-  // connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DB
-  // debug: false
 });
 
 connection.connect();
